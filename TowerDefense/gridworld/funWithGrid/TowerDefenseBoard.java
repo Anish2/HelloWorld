e6ad111f79a618a3bbb775fc9a214e848d7306c4
321077 <321077@HSS-MC2-02.westwindsor.k12.nj.us>
2014-03-27 11:31:46 -0400

    st

--- TowerDefense/gridworld/funWithGrid/TowerDefenseBoard.java
+++ TowerDefense/gridworld/funWithGrid/TowerDefenseBoard.java
@@ -8,20 +8,29 @@ import info.gridworld.world.World;
 public class TowerDefenseBoard extends World<TowerDefenseObject>
 {
 	private Tower currentTowerToBuild; //Set this to a default once i make a basic tower
-	private int gold;// Decide on starting gold
+	private int gold = 200;// Decide on starting gold
 	private int wave = 1; // The current wave
-	private int lives; // Decide on starting lives
+	private int lives = 10; // Decide on starting lives
 	private ArrayList<Monster> toBeDeployed; // The monsters that are waiting to be deployed
-	private ArrayList<Monster> inGrid; // The monsters that are already in the grid
-	
+	private ArrayList<Monster> inGrid; // The monsters that are already in the grid ///////
+
 	public boolean keyPressed(String description, Location loc)
 	{
 		System.out.println(description);
 		return false;
 	}
-	
+
 	public boolean locationClicked(Location loc)
 	{
 		return false;
+	}
+
+	public TowerDefenseBoard()
+	{
+		toBeDeployed = new ArrayList<Monster>();
+		inGrid = new ArrayList<Monster>();
+		super.setMessage("Gold :" + gold + " Lives : " + lives + "Wave :" + wave + "\n" + "Current Tower Type :" + currentTowerToBuild.getClass() + " To Change This Tower Type, Press b for basic tower, s for splash tower, and l for long range 
+
+				tower" );
 	}
 }
