a40debfcb0abee2e8a54798e7841508aa57ad238
avi <avi@192.168.1.137>
2014-04-02 18:50:11 -0400

    s

--- TowerDefense/gridworld/funWithGrid/TowerDefenseRunner.java
+++ TowerDefense/gridworld/funWithGrid/TowerDefenseRunner.java
@@ -1,7 +1,5 @@
 package funWithGrid;
 
-import info.gridworld.grid.Location;
-
 public class TowerDefenseRunner 
 {
 	public static void main(String[] args)
@@ -9,13 +7,6 @@ public class TowerDefenseRunner
 		TowerDefenseBoard t = new TowerDefenseBoard();
 		
 		t.show();
-<<<<<<< HEAD
-		
-		Tower tow = new Tower();
-		tow.putSelfInGrid(t.getGrid(), new Location(4,5));
-		
-=======
 		t.generateRandomField();
->>>>>>> branch 'master' of https://github.com/poidude7/HelloWorld.git
 	}
 }
