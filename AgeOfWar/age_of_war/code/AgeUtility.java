f8f154c38ea6512ac23d2d9d6e6882e4ab45e45b
176133 <176133@HSS-101-07.westwindsor.k12.nj.us>
2014-09-24 13:33:00 -0400

    AgeUtil changes

--- AgeOfWar/age_of_war/code/AgeUtility.java
+++ AgeOfWar/age_of_war/code/AgeUtility.java
@@ -9,6 +9,19 @@ import processing.core.PImage;
  */
 public class AgeUtility 
 {
+	public static final int CLUB_MAN = 0;
+	public static final int ARCHER = 1;
+	public static final int NINJA = 2;
+	public static final int ALI_BABA = 3;
+	
+	public static final int DARK = 4;
+	public static final int MEDIEVAL = 5;
+	
+	public static final int EARTH_YAGURA = 6;
+	public static final int WIND_YAGURA = 7;
+	
+	
+	
 	/**
 	 * Returns Unit with characteristics according to type.
 	 * @param type type of unit
