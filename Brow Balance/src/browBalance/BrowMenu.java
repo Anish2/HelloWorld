c1f40a9e2c7b54552ccdec1175bd6fd154eaab6e
poidude7 <zlatineitan@gmail.com>
2015-06-08 11:54:38 -0400

    Test

--- Brow Balance/src/browBalance/BrowMenu.java
+++ Brow Balance/src/browBalance/BrowMenu.java
@@ -4,9 +4,5 @@ import processing.core.PApplet;
 
 public class BrowMenu extends PApplet
 {
-	public static void main(String[] args)
-	{
-		BrowDisplay brow = new BrowDisplay(1,100,0);
-		brow.paint(createGraphics(40,40));
-	}
+	
 }
