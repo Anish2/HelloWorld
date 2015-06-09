ace1bb76b588952cbfe4a9e5b6216a2ff0499ce8
anishvisagit <anishvisaria98@gmail.com>
2015-06-08 23:56:15 -0400

    tsunami dash

--- Brow Balance/src/browBalance/BrowDisplay.java
+++ Brow Balance/src/browBalance/BrowDisplay.java
@@ -16,6 +16,7 @@ public class BrowDisplay extends PApplet
 	private FWorld world;
 	private FBox brow;
 	private ArrayList<FBox> goals = new ArrayList<FBox>();
+	private ArrayList<FBody> balls = new ArrayList<FBody>();
 	private int numGoals = 3;
 	private int lives = 100;
 	private int score = 0;
@@ -28,39 +29,47 @@ public class BrowDisplay extends PApplet
 	private int numBalls = 0;
 	private int maxBalls = 1;
 	private ArrayList<FBox> obstacles = new ArrayList<FBox>();
-	//private boolean levelingUp = false; 
 	private int speed = 5;
+	private float yoff = (float) 0.0; 
+	private float water_level;
 
-	/*public BrowDisplay(int level, int lives, int score)
-	{
-		this.lives = lives;
-		this.score = score;
-		this.level = level;
-	}*/
-	
-/*	private void levelUpDraw()
-	{
-		background(0);
-		PFont font = createFont("Arial",40);
-		textFont(font);
-		
-		
-		
-		brow = null;
-		goals = new ArrayList<FBox>();
-		level++;
-		setup();
-	}*/
-	
+
 	public void drawNormal()
 	{
-		background(255);
+		background(240);
+
+		fill(color(204, 102, 0));
+		// We are going to draw a polygon out of the wave points
+		beginShape(); 
+
+		float xoff = 0;       // Option #1: 2D Noise
+		// float xoff = yoff; // Option #2: 1D Noise
+
+		// Iterate over horizontal pixels
+		for (float x = 0; x <= width; x += 10) {
+			// Calculate a y value according to noise, map to 
+			float y = map(noise(xoff, yoff), 0, 1, water_level-40,water_level); // Option #1: 2D Noise
+			// float y = map(noise(xoff), 0, 1, 200,300);    // Option #2: 1D Noise
+			
+			// Set the vertex
+			vertex(x, y-10); 
+			// Increment x dimension for noise
+			xoff += 0.08;
+		}
+		// increment y dimension for noise
+		yoff += 0.01;
+		vertex(width, height);
+		vertex(0, height);
+		endShape(CLOSE);
+
+		//background(255);
 		fill(0);
+		water_level -= 0.01;
 		text("Lives : " + (lives), 15, 30);
 		text("Score : " + score, width - 100, 30);
 		text("Level : " + level, width - 200,30);
-		
-		
+
+
 		if (frameCount % framesPerBall == 0 && numBalls < maxBalls) 
 		{
 			FCircle b = new FCircle(20);
@@ -83,24 +92,25 @@ public class BrowDisplay extends PApplet
 
 			numBalls++;
 			world.add(b);
+			balls.add(b);
 		}
 
 		handleBrowMovement();
 		updateScore();
+		handleTsunamiEffect();
 		updateLives();
-
 		world.draw();
 		world.step();
 
 		strokeWeight(1);
 		stroke(255);
 	}
-	
+
 	public void setup() 
 	{
-		size(800, 500);
+		size(640, 360);
 		smooth();
-
+		water_level = height-10;
 		Fisica.init(this);
 
 		world = new FWorld();
@@ -133,7 +143,7 @@ public class BrowDisplay extends PApplet
 				posNums.remove(new Integer(y));
 			}
 			/*goal.setPosition(pos, height - 30); */
-			goal.setPosition(pos,height - 30); 
+			goal.setPosition(pos,height - 60); 
 			goal.setStatic(true);
 			goal.setRestitution(1.0f);
 			goal.setGrabbable(false);
@@ -152,12 +162,20 @@ public class BrowDisplay extends PApplet
 
 	public void draw()
 	{
-		/*if(levelingUp)
-		{
-			levelUpDraw();
-		}*/
 		maxBalls = score/20 + 1;
 		drawNormal();
+	}
+	
+	public void handleTsunamiEffect() {
+		for (int i = 0; i < balls.size(); i++) {
+			if (balls.get(i).getY() >= water_level-20) {
+				//System.out.println("in here");
+				if (Math.random() > 0.5)
+					balls.get(i).addForce(random(850,1000), random(-1000,-575));
+				else
+					balls.get(i).addForce(random(-850,-1000), random(-1000,-575));
+			}
+		}
 	}
 
 	public void handleBrowMovement()
@@ -187,13 +205,8 @@ public class BrowDisplay extends PApplet
 		{
 			for(FContact a : (ArrayList<FContact>)goals.get(x).getContacts())
 			{
-				if(a.getBody1() instanceof FCircle && ((FCircle) a.getBody1()).getSize() == 20 || a.getBody2() instanceof FCircle && ((FCircle) a.getBody2()).getSize() == 20 )
-				{
-					// Regular size
-				}
 				if(goals.get(x).equals(a.getBody2()) && a.getBody1().getFillColor() == goals.get(x).getFillColor())
 				{
-					
 					popBall(a.getBody1());
 					score++;
 				}
@@ -205,10 +218,9 @@ public class BrowDisplay extends PApplet
 			}
 		}
 	}
-	
+
 	public void popBall(FBody ball)
 	{
-		
 		int particles = (int)random(10,30);
 		for(int x = 0; x < particles; x++)
 		{
@@ -222,9 +234,10 @@ public class BrowDisplay extends PApplet
 			world.add(particle);
 		}
 		world.remove(ball);
+		balls.remove(ball);
 		numBalls--;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void updateLives()
 	{
@@ -241,13 +254,14 @@ public class BrowDisplay extends PApplet
 				if((a.getX() < -20 || a.getX() > width + 20 || a.getY() < -20 || a.getY() > height + 20))
 				{
 					world.remove(a);
+					balls.remove(a);
 					numBalls--;
 					lives--;
 				}
 			}
 		}
 	}
-	
+
 	public void keyReleased()
 	{
 		if (key == CODED)
@@ -294,6 +308,6 @@ public class BrowDisplay extends PApplet
 		}
 	}
 
-	
+
 
 }
